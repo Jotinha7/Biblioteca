973f29602af6fb660fc5f00b0eb85713d66d7257
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2023-02-11 17:06:38 -0300

    Create Dsu.cpp

---
+++ Codigo/Estruturas/Dsu.cpp
@@ -0,0 +1,55 @@
+class DSU {
+  public:
+    int n;
+    vector<int> id, sz;
+
+    DSU(int _n) : n(_n) {
+      sz.assign(n, 1);
+      id.resize(n);
+      iota(id.begin(), id.end(), 0);
+    }
+
+    int find(int x) {
+      return id[x] = (id[x] == x ? x : find(id[x]));
+    }
+
+    void uni(int x, int y) {
+        x = find(x), y = find(y);
+        if(x == y) return;
+        if(sz[x] > sz[y]) swap(x,y);
+        id[x] = y;
+        sz[y] += sz[x];
+    }
+};
+
+//salva o vetor dos componentes
+
+class DSU {
+  public:
+    int n;
+    vector<int> id;
+    vector<vector<int>> sz;
+
+    DSU(int _n) : n(_n) {
+      sz = vector<vector<int>> (n);
+      for(int i = 0; i < n; i++) {
+        sz[i].push_back(i);
+      }
+      id.resize(n);
+      iota(id.begin(), id.end(), 0);
+    }
+    
+    int find(int x) {
+      return id[x] = (id[x] == x ? x : find(id[x]));
+    }
+
+    void uni(int x, int y) {
+        x = find(x), y = find(y);
+        if(x == y) return;
+        if(sz[x].size() > sz[y].size()) swap(x,y);
+        id[x] = y;
+        for(auto x : sz[x]) {
+          sz[y].push_back(x);
+        }
+    }
+};
