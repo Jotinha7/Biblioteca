e4252f5ca56d7ea862ec0370476dccf65f8c2672
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2023-02-11 17:08:40 -0300

    Update BIT.cpp

--- Codigo/Estruturas/BIT.cpp
+++ Codigo/Estruturas/BIT.cpp
@@ -1,23 +1,23 @@
 class FenwickTree {
   public:
-    void init(int N) {
-      n = N;
-      BIT.assign(n + 1, 0ll);
+    int n;
+    vector<int> BIT;
+
+    FenwickTree(int _n) : n(_n) {
+      BIT = vector<int> (n + 1);
     }
+
     void update(int i, int x) {
       for (int j = i; j <= n; j += j & -j)
         BIT[j] += x;
     }
+
     int getSum(int i) {
       int sum = 0;
       for (int j = i; j > 0; j -= j & -j)	
         sum += BIT[j];
       return sum;
     }
+
     int query(int l, int r) { return getSum(r) - getSum(l - 1); }
-
-  private:
-
-    int n;
-    vector<int> BIT;
 };
