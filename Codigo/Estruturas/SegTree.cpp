7719becc219701460207b2d59b641cee4a2f597d
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2023-02-11 17:19:10 -0300

    Update SegTree.cpp

--- Codigo/Estruturas/SegTree.cpp
+++ Codigo/Estruturas/SegTree.cpp
@@ -5,6 +5,7 @@ namespace seg {
   int combina(int a, int b) {
     return a + b;
   }
+  
   int build(int p = 1, int l = 0, int r = n - 1) {
     lazy[p] = 0;
     if (l == r) {
@@ -13,10 +14,12 @@ namespace seg {
     int m = (l+r)/2;
     return seg[p] = combina(build(2 * p, l, m), build(2 * p + 1, m + 1, r));
   }
+  
   void build(int n2, vector<int>& v2) {
     n = n2, a = v2;
     build();
   }
+  
   void prop(int p, int l, int r) {
     if(lazy[p]) {
       seg[p] = lazy[p];
@@ -27,6 +30,7 @@ namespace seg {
     }
     lazy[p] = 0;
   }
+  
   int query(int a, int b, int p = 1, int l = 0, int r = n - 1) {
     prop(p, l, r);
     if (a <= l and r <= b) return seg[p];
@@ -34,6 +38,7 @@ namespace seg {
     int m = (l + r) / 2;
     return combina(query(a, b, 2 * p, l, m), query(a, b, 2 * p + 1, m + 1, r));
   }
+  
   int update(int a, int b, int x, int p = 1, int l = 0, int r = n - 1) {
     prop(p, l, r);
     if (a <= l and r <= b) {
