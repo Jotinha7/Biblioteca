e7fd4614dca56619f373980b97c8d359df6cb46b
ricaxov <103327245+ricaxov@users.noreply.github.com>
2023-02-14 19:59:31 -0300

    Create ricaxov_template.cpp

---
+++ Codigo/ricaxov_template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define endl '\n'
+#define pb push_back
+#define tos to_string
+#define mod 998244353
+#define inf 1000000000
+#define pi 3.141592653589793
+#define linf 1000000000000000000
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define entrada freopen("input.txt", "r", stdin);
+#define saida freopen("output.txt", "w", stdout);
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+void dbg() { cerr << endl; }
+template<typename X, typename...Y> void dbg(X x, Y...y) { cerr << ' ' << x; dbg(y...); }
+#define debug(...) cerr << "(" << #__VA_ARGS__ << "):", dbg(__VA_ARGS__), cerr << endl
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+  int tt = 1; cin >> tt;
+  while(tt--) {
+    
+  }
+}
