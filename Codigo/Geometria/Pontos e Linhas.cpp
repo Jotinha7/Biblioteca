19ffc4be3eaf5d0ba992b4f98e7425739bf5f53c
ricaxov <103327245+ricaxov@users.noreply.github.com>
2023-02-16 21:38:27 -0300

    Update Pontos e Linhas.cpp

--- Codigo/Geometria/Pontos e Linhas.cpp
+++ Codigo/Geometria/Pontos e Linhas.cpp
@@ -54,10 +54,71 @@ struct line {
     c = A.y * B.x - A.x * B.y;
   }
 };
+
 ld dist(const point& p, const line& l) { // distancia minima entre um ponto e uma linha ponto para uma reta
   return fabs((l.a * p.x) + (l.b * p.y) + l.c) / sqrt(l.a * l.a + l.b * l.b);
 }
 
+void slope_to_line(const point& p, ld slope, line& l) { // converte do tipo slope (y = ax + b) para o tipo standard (ax + by = c)
+  l = {-slope, 1.0, -slope * p.x + p.y};
+}
+
+bool parallel(const line& l1, const line& l2) { // check parallel
+  return (fabs(l1.a - l2.a) < eps) && (fabs(l1.b - l2.b) < eps);
+}
+bool same(const line& l1, const line& l2) { // check same line
+  return parallel(l1, l2) && (fabs(l1.c - l2.c) < eps);
+}
+
+bool intersect(const line& l1, const line& l2, point& p) { // return 1 se as retas se intersectao => intersection at point p
+  if(parallel(l1, l2)) {
+    return 0;
+  }
+  p.x = (l2.b * l1.c - l1.b * l2.c) / (l2.a * l1.b - l1.a * l2.b);
+  if(fabs(l1.b) > eps) {
+    p.y = -(l1.a * p.x + l1.c);
+  }
+  else {
+    p.y = -(l2.a * p.x + l2.c);
+  }
+  return 1;
+}
+
+struct vec {
+  ld x, y;
+  vec(){ x = y = 0.0; }
+  vec(ld X, ld Y) { x = X, y = Y; }
+  vec(point A, point B) {
+    x = B.x - A.x;
+    y = B.y - A.y;
+  }
+};
+
+
+/* Depois precisa dar uma olhada melhor nisso aqui
+vec scale(const vec &v, double s) { // s = [<1..1..>1]
+  return vec(v.x*s, v.y*s); // shorter/eq/longer
+} // return a new vec
+point translate(const point &p, const vec &v) { // translate p
+  return point(p.x+v.x, p.y+v.y); // according to v
+} // return a new point
+*/
+
+/* uva - 10263
+double distToLineSegment(point p, point a, point b, point &c) {
+vec ap = toVec(a, p), ab = toVec(a, b);
+double u = dot(ap, ab) / norm_sq(ab);
+if (u < 0.0) { // closer to a
+c = point(a.x, a.y);
+return dist(p, a); // dist p to a
+}
+if (u > 1.0) { // closer to b
+c = point(b.x, b.y);
+return dist(p, b); // dist p to b
+}
+return distToLine(p, a, b, c); // use distToLine
+}
+*/
 int main() {
   ios_base::sync_with_stdio(false); cin.tie(NULL);
   
