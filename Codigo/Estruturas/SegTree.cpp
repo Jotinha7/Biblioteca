57146a250d281359848491755c153103d0f6e629
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2023-04-22 21:20:57 -0300

    Update SegTree.cpp

--- Codigo/Estruturas/SegTree.cpp
+++ Codigo/Estruturas/SegTree.cpp
@@ -1,23 +1,24 @@
 namespace seg {
-  vector<int> seg, lazy, a;
+  vector<long long> seg, lazy;
+  vector<int> a;
   int n;
 
-  int combina(int a, int b) {
+  long long combina(int a, int b) {
     return a + b;
   }
   
-  int build(int p = 1, int l = 0, int r = n - 1) {
+  long long build(int p = 1, int l = 0, int r = n - 1) {
     lazy[p] = 0;
     if (l == r) {
       return seg[p] = a[l];
     }
-    int m = (l+r)/2;
+    int m = (l + r) / 2;
     return seg[p] = combina(build(2 * p, l, m), build(2 * p + 1, m + 1, r));
   }
   
   void build(int n2, vector<int>& v2) {
     n = n2, a = v2;
-    seg = lazy = vector<int> (4 * n);
+    seg = lazy = vector<long long> (4 * n);
     build();
   }
   
@@ -32,15 +33,19 @@ namespace seg {
     lazy[p] = 0;
   }
   
-  int query(int a, int b, int p = 1, int l = 0, int r = n - 1) {
+  long long query(int a, int b, int p = 1, int l = 0, int r = n - 1) {
     prop(p, l, r);
-    if (a <= l and r <= b) return seg[p];
-    if (b < l or r < a) return 0;
+    if (a <= l and r <= b) {
+      return seg[p];
+    }
+    if (b < l or r < a) {
+      return 0;
+    }
     int m = (l + r) / 2;
     return combina(query(a, b, 2 * p, l, m), query(a, b, 2 * p + 1, m + 1, r));
   }
   
-  int update(int a, int b, int x, int p = 1, int l = 0, int r = n - 1) {
+  long long update(int a, int b, int x, int p = 1, int l = 0, int r = n - 1) {
     prop(p, l, r);
     if (a <= l and r <= b) {
       lazy[p] += x;
@@ -51,7 +56,7 @@ namespace seg {
       return seg[p];
     }
     int m = (l + r) / 2;
-    return seg[p] = combina(update(a, b, x, 2*p, l, m),
+    return seg[p] = combina(update(a, b, x, 2 * p, l, m),
       update(a, b, x, 2 * p + 1, m + 1, r));
   }
 };
