2c8919ab0a7425641a3743d6ac2400609fa129fb
ricaxov <103327245+ricaxov@users.noreply.github.com>
2023-05-02 08:40:55 -0300

    Update ricaxov_template.cpp

--- Codigo/ricaxov_template.cpp
+++ Codigo/ricaxov_template.cpp
@@ -25,10 +25,10 @@ using iii = array<int, 3>;
 #define xov "┌( ಠ_ಠ)┘"
 #define mem(x, y) memset(x, y, sizeof(x))
 #define inbounds(x, l, r) (l <= x && x <= r)
-#define L1(var...) [&](auto x) { return var; }
-#define L2(var...) [&](auto x, auto y) { return var; }
 #define fillin freopen("in.txt", "r", stdin)
 #define fillout freopen("out.txt", "w", stdout)
+#define L1(var...) [&](auto x) { return var; }
+#define L2(var...) [&](auto x, auto y) { return var; }
 
 template<class X, class Y> auto& operator >> (istream& is, pair<X, Y>& var) { is >> var.ff >> var.ss; return is; }
 template<class X, class Y> auto& operator << (ostream& os, pair<X, Y> const& var) { os << "(" << var.ff << " " << var.ss << ")"; return os; }
