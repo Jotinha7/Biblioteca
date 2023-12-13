7fc3f32318fb32b3afb5f3389f7c0e5b862f04c4
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2023-12-13 07:31:18 -0300

    Create redes.c

---
+++ redes.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+struct meio{
+	int livre;
+	int mensagem;
+};
+
+int main(){
+	struct meio *at = malloc(sizeof(struct meio));
+	int id = shmget(IPC_PRIVATE, sizeof(at), IPC_CREAT | 0666);
+	at = (struct meio*) shmat(id, NULL, 0);
+  at->livre=1;
+  fork();
+  srandom(getpid());
+  while(1){
+    while(!(at->livre));
+    at->livre = 0;
+    int cc = random()%100;
+    at->mensagem = cc;
+    printf("id: %d   atual: %d\n", getpid(), at->mensagem);
+    if(cc != at->mensagem) {
+      printf("colidiu com id: %d\n", getpid());
+    }
+    at->livre = 1;
+    sleep(3);
+  }
+  at->livre=1;
+  sleep(1);
+}
