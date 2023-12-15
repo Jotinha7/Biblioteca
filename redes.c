f16742abf1fd027d3be424bb80b2e295baa9cec5
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2023-12-15 06:49:40 -0300

    redes.c

--- redes.c
+++ redes.c
@@ -6,30 +6,63 @@
 #include <sys/wait.h>
 #include <stdio.h>
 
+struct pacote {
+	int destino[6];
+	int origem[6];
+	int dados[1500];
+};
+
 struct meio{
   int livre;
-  int mensagem;
+  struct pacote atual;
 };
 
 int main(){
   struct meio *at = malloc(sizeof(struct meio));
   int id = shmget(IPC_PRIVATE, sizeof(at), IPC_CREAT | 0666);
+  if(id < 0) {
+    printf("nao alocado\n");
+  }
   at = (struct meio*) shmat(id, NULL, 0);
   at->livre=1;
-  fork();
-  srandom(getpid());
-  while(1){
-    while(!(at->livre));
-    at->livre = 0;
-    int cc = random()%100;
-    at->mensagem = cc;
-    printf("id: %d   atual: %d\n", getpid(), at->mensagem);
-    if(cc != at->mensagem) {
-      printf("colidiu com id: %d\n", getpid());
+  pid_t k = fork();
+  if(k == 0) {
+    for(int i = 0; i < 8; i++) {
+      fork();
     }
-    at->livre = 1;
-    sleep(3);
+    
+    srandom(getpid());
+    
+    while(1){
+      while(!(at->livre));
+      at->livre = 0;
+      struct pacote guarda;
+      for (int i = 0; i < 6; i++) {
+				guarda.destino[i] = (rand() % 256);
+				guarda.origem[i] = (rand() % 256);
+			}
+			for (int i = 0; i < 1500; i++) {
+				guarda.dados[i] = 256 + rand() % 100000;
+			}
+			at->atual = guarda;
+			printf("origem atual: ");
+			for (int i = 0; i < 6; i++) {
+				printf("%d ", guarda.origem[i]);
+			}
+			printf("\ndestino atual: ");
+			for (int i = 0; i < 6; i++) {
+				printf("%d ", guarda.destino[i]);
+			}
+			
+      if(at->atual.dados != guarda.dados) {
+        printf("colidiu com id: %d\n", getpid());
+      }
+      at->livre = 1;
+      sleep(3);
+    }
+    at->livre=1;
+    sleep(rand() % 5);
+  } else {
+    printf("processo pai\n");
   }
-  at->livre=1;
-  sleep(1);
 }
