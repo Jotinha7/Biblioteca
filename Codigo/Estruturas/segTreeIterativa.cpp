91cb6a978e68d4e9927ff00768dc9e3a1903b3b1
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2025-02-15 17:25:28 -0300

    Update segTreeIterativa.cpp

--- Codigo/Estruturas/segTreeIterativa.cpp
+++ Codigo/Estruturas/segTreeIterativa.cpp
@@ -14,23 +14,23 @@ template<typename T> struct segtree {
   T neutral;
   vector<T> t;
   function<T(T, T)> merge;
-
+ 
   segtree(vector<int> v, T _e, function<T(T, T)> f) : n((int) v.size()), merge(f), neutral(_e) {
     t = vector<T> (2 * n);
     for(int i = 0; i < n; i++) {
-      t[i + n] = {v[i], 1};
+      t[i + n] = v[i];
     }
     for (int i = n - 1; i > 0; --i) {
       t[i] = merge(t[i << 1], t[(i << 1) | 1]);
     }
   }
-
+ 
   void update(int p, T value) {
     for (t[p += n] = value; p > 1; p >>= 1) {
       t[p >> 1] = merge(t[p], t[p ^ 1]);
     }
   }
-
+ 
   T query(int l, int r) {
     T res = neutral;
     for (l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
