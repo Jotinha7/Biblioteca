132c16a7c5b497c57e8f7b48d13cb174ff3393d5
Jotinha7 <106353027+Jotinha7@users.noreply.github.com>
2025-03-01 20:07:34 -0300

    Create segTreeEsparsa.cpp

---
+++ Codigo/Estruturas/segTreeEsparsa.cpp
@@ -0,0 +1,64 @@
+// SegTree Esparsa
+//
+// Query: soma do range [i, j]
+// Update: soma x em i
+// O MAX tem q ser Q log N(quantos updates vai ter) para Q updates
+// Contrutor: a(menor index que vou mudar), b(maior index que vou mudar)
+//
+// Complexidades:
+// build - O(1)
+// query - O(log(n))
+// update - O(log(n))
+
+template<typename T> struct dynamic_seg {
+  int t;
+  T neutral, a, b;
+  T seg[MAX], esq[MAX], dir[MAX];
+  function<T(T, T)> merge;
+ 
+  dynamic_seg(T _a, T _b, T _neutral, function<T(T, T)> f) : a(_a), b(_b), neutral(_neutral), merge(f) {
+    seg[0] = seg[1] = neutral;
+    esq[0] = dir[0] = esq[1] = dir[1] = 0;
+    t = 2;
+  }
+ 
+  int create() {
+    seg[t] = neutral;
+    esq[t] = dir[t] = 0;
+    return t++;
+  }
+ 
+  T update(T i, T x, int p, T l, T r) {
+    if(i < l or i > r) {
+      return seg[p];
+    }
+    if(l == r) {//
+      return seg[p] += x;
+    }
+    int mid = (l + r) >> 1; // tem que ser assim quando o range é negativo
+    if(i <= mid) {
+      if(esq[p] == 0) {
+        esq[p] = create();
+      }
+    } else {
+      if(dir[p] == 0) {
+        dir[p] = create();
+      }
+    }
+    return seg[p] = merge(update(i, x, esq[p], l, mid), update(i, x, dir[p], mid + 1, r));
+  }
+ 
+  T query(T i, T j, int p, T l, T r) {
+    if(p == 0 or r < i or l > j) {
+      return neutral;
+    }
+    if(l >= i && r <= j) {
+      return seg[p];
+    }
+    int mid = (l + r) >> 1;
+    return merge(query(i, j, esq[p], l, mid), query(i, j, dir[p], mid + 1, r));
+  }
+  
+  T update(T i, T x) { return update(i, x, 1, a, b); }
+  T query(T i, T j) { return query(i, j, 1, a, b); }
+};
